6672446e1d611417ed56b7777f57f203e4ee2fa4
winstone-1 <winstonemuna404@gmail.com>
2026-03-01 16:32:11 +0300

    Get Reference to DOM Elements

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,6 @@
-// 
+// DOM ELEMENTS
+const FoodInput = document.getElementById('Food-Item');
+const FoodType = document.getElementById('Food-Type');
+const CaloriesInput = document.getElementById('Calories-Input');
+const SaveButton = document.querySelector('button[type="submit"]');
+const ProgressBar = document.querySelector('.bg-emerald-500.h-2.rounded-full');
